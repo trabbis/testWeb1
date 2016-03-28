7d3d9767f81392ef8491498ad23004a08652e019
dchoi <dchoi@PS-DCHOI.nbspaymentsolutions.com>
2016-03-28 17:22:46 -0400

    added angular

---
+++ WebContent/angular01/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('myapp', []);
+
+app.controller('MainCtrl', function($scope) {
+  
+});
+
+app.directive('helloWorld',function(){
+  return{
+    restrict: 'AE',
+    replace: true,
+    template: '<p style="background-color:{{color}}">Hello World</p>',
+    link: function(scope,elem,attrs){
+      elem.bind('click',function(){
+        elem.css('background-color','white');
+        scope.$apply(function(){
+          scope.color="white";
+        });
+      });
+      elem.bind('mouseover',function(){
+        elem.css('cursor','pointer');
+      });
+    }
+  }
+});
